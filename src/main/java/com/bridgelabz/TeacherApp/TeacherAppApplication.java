5ff2ef104aa2717f2228ae53cc7d68c92314ba65
sheetal-10 <sheetalp257@gmail.com>
2021-10-09 22:33:56 +0530

    Added: project files form spring initializer

---
+++ src/main/java/com/bridgelabz/TeacherApp/TeacherAppApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.TeacherApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeacherAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/TeacherApp/TeacherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.TeacherApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
